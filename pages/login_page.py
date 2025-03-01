1040519746745ee752e9d634fc1bd93f54dbf259
rajeshpadhy <rpadhy98@gmail.com>
2025-03-01 08:29:50 +0530

    updated the test script for all the condition

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,7 @@ class LoginPage(SeleniumDriver):
     email_field_XPATH = "//input[@id='email1']"
     password_field_XPATH = "//input[@id='password1']"
     login_button_XPATH = "//button[normalize-space()='Sign in']"
+    login_error_message_xpath = "//h2[@class='errorMessage']"
 
     def __init__(self,driver):
         super().__init__(driver)
@@ -17,6 +18,10 @@ class LoginPage(SeleniumDriver):
         self.type_element(email,self.email_field_XPATH,"xpath")
         self.type_element(password,self.password_field_XPATH,"xpath")
         self.click_element(self.login_button_XPATH,"xpath")
+
+    def login_error_message_for_invalid_mail_and_password_text(self):
+        return self.get_element(self.login_error_message_xpath,"xpath").text
+
 
 
 
